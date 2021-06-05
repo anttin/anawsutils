ea8dfa343955d40df19cb7efdcc95746954a5881
Antti Nilsson <muut.py@antion.fi>
2021-06-05 14:16:51 +0300

    v0.1.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name="anawsutils",
-  version="0.1.4",
+  version="0.1.5",
   author="anttin",
   author_email="muut.py@antion.fi",
   description="Module with miscallaneous AWS utils.",
