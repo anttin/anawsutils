5634029c66699bd5cfb0f0237fa04bdef10776fd
Antti Nilsson <muut.py@antion.fi>
2021-06-05 14:13:11 +0300

    Search hostedzone by dnsname

--- anawsutils/r53.py
+++ anawsutils/r53.py
@@ -6,13 +6,14 @@ import boto3
 class Route53(object):
   def __init__(self):
     self.client = boto3.client("route53")
-    self.hostedzones = {}
+    self._cache_hostedzones = {}
+    self._cache_dnsnames = {}
 
 
   @staticmethod
   def _ensure_trailing_dot(s):
     if s is None or not isinstance(s, str) or len(s) < 1:
-      raise ValueError("Invalid domain name")
+      raise ValueError("Invalid input")
     elif s[-1] == '.':
       return s
     else:
@@ -27,18 +28,62 @@ class Route53(object):
   def get_hosted_zone_id_by_domain(self, domain_name, cache=True):
     _dn = self._ensure_trailing_dot(domain_name)
 
-    if cache is True and _dn in self.hostedzones:
-      return self.hostedzones[_dn]
+    if cache is True and _dn in self._cache_hostedzones:
+      return self._cache_hostedzones[_dn]
 
     r = self.list_hosted_zones_by_name(_dn)
 
     hzid = None
 
     for z in r["HostedZones"]:
-      if cache is True and z["Name"] not in self.hostedzones:
-        self.hostedzones[z["Name"]] = z["Id"]
+      if cache is True and z["Name"] not in self._cache_hostedzones:
+        self._cache_hostedzones[z["Name"]] = z["Id"]
       if z["Name"] == _dn:
         hzid = z["Id"]
+        break
+
+    return hzid
+
+
+  @staticmethod
+  def _get_domain(dnsname, level=2):
+    _dnsname = dnsname[:-1] if dnsname[-1] == '.' else dnsname
+    a = _dnsname.split('.')
+    r = '.'.join(a[-level:]) + '.'
+    return r
+
+
+  @classmethod
+  def _get_possible_zones(cls, dnsname):
+    _dnsname = dnsname[:-1] if dnsname[-1] == '.' else dnsname
+    a = _dnsname.split('.')
+    r = []
+    for i in range(2, len(a)+1):
+      r.append(cls._get_domain(dnsname, i))
+    return r
+
+
+  def get_hosted_zone_id_by_dnsname(self, dnsname, cache=True):
+    _dnsn = self._ensure_trailing_dot(dnsname)
+    if cache is True and dnsname in self._cache_dnsnames:
+      return self._cache_dnsnames[_dnsn]
+
+    possible_zones = self._get_possible_zones(dnsname)
+    dn = self._get_domain(dnsname)
+    r = self.list_hosted_zones_by_name(dn)
+
+    hzid = None
+
+    zones = {}
+    for z in r["HostedZones"]:
+      zones[z["Name"]] = z["Id"]
+      if cache is True and z["Name"] not in self._cache_hostedzones:
+        self._cache_hostedzones[z["Name"]] = z["Id"]
+
+    possible_zones.reverse()
+    for z in possible_zones:
+      if z in zones:
+        hzid = zones[z]
         break
 
     return hzid
